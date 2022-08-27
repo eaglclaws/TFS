dabeb0e441b56bdd970b87036b03d2fe16a7fa1f
Joseph Ko <eaglclaws@gmail.com>
2022-08-27 13:44:59 +0900

    ./tag returns error on non-existant tag or file

--- src/tag.c
+++ src/tag.c
@@ -15,6 +15,7 @@ main(int argc, char *argv[])
 	char sql[1000];
 	int rc;
 	sqlite3 *db;
+	sqlite3_stmt *stmt;
 	FILE *dbfile;
 
 	if (argc < 3 || (argc > 2 && strlen(argv[1]) != 64)) {
@@ -25,11 +26,35 @@ main(int argc, char *argv[])
 	if (dbfile == NULL) {
 		fclose(dbfile);
 		fprintf(stderr, "Database not found, are you sure you are in tfs?\n");
-		return 0;
+		return -1;
 	}
 	fclose(dbfile);
 	rc = sqlite3_open("../tfs.db", &db);
 
+	dbfile = fopen(argv[1], "r");
+	if (dbfile == NULL) {
+		fclose(dbfile);
+		fprintf(stderr, "File does not exist\n");
+		return -1;
+	}
+	fclose(dbfile);
+
+	char counter[1000] = "SELECT COUNT(name) FROM tags WHERE ";
+	for (int i = 2; i < argc; i++) {
+		char buff[100];
+		sprintf(buff, "name=\'%s\'", argv[i]);
+		strcat(counter, buff);
+		if (i != argc - 1) {
+			strcat(counter, " OR ");
+		}
+	}
+	strcat(counter, ";");
+	sqlite3_prepare_v2(db, counter, -1, &stmt, NULL);
+	sqlite3_step(stmt);
+	if (argc - 2 != sqlite3_column_int(stmt, 0)) {
+		printf("A tag does not exist\n");
+		return -1;
+	}
 	for (int i = 2; i < argc; i++) {
 		sprintf(sql, "INSERT INTO tagmap VALUES(\'%s\', \'%s\');", argv[1], argv[i]);
 		rc = sqlite3_exec(db, sql, NULL, NULL, &zErrMsg);
